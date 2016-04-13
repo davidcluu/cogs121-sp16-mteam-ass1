f19e46ed22d5cf6766d4eb2a417cc838e2f11d71
Christine Do <christinevdo@gmail.com>
2016-04-13 12:17:11 -0700

    changed the order videos are displayed (newest to oldest)

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var models = require("../models");
 exports.view = function(req, res) {
   models.Comment
     .find()
+    .sort('-posted')
     .exec(function(err, comments) {
       console.log(comments);
       var commentFeed = {commentFeed : comments};
