fbf5f2ab0c0a5a48db524cc3b481bccf16f14497
David Luu <luu.david.c@gmail.com>
2016-04-19 00:02:18 -0700

    heroku rename

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ var strategy = {
 
 // Database Connection
 var db = mongoose.connection;
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://127.0.0.1/cogs121');
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI || 'mongodb://127.0.0.1/cogs121');
 db.on('error', console.error.bind(console, 'Mongo DB Connection Error:'));
 db.once('open', function(callback) {
     console.log("Database connected successfully.");
